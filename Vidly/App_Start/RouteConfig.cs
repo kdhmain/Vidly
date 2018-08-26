410c8626a9fd693851376eefea3a9c016800ebf2
kdhmain <marckopolo4real@gmail.com>
2018-08-26 07:48:10 -0400

    Bootstrap theme, route mapping, customer controller and viewmodel

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,14 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    "MoviesByReleasedDate",
+            //    "movies/released/{year}/{month}",
+            //    new {controller="Movies",action="ByReleasedDate"},
+            //    new {year=@"d{4}",month = @"\d{2}"}
+            //    );
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            return View(movie);
+        }
+
+        //[Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+        //public ActionResult ByReleasedDate(int year,int month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
+
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("id=" + id);
+        //}
+
+        ////movies
+        //public ActionResult Index(int? pageIndex,string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+
+        //    return Content(String.Format("pageIndex={0}&sortby={1}",pageIndex,sortBy));
+        //}
+    }
+}
