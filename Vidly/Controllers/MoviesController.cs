ed4cf30075d773e688916e43dd7afa1c6c4d1953
kdhmain <marckopolo4real@gmail.com>
2018-08-31 03:15:56 -0400

    Display the list of movies and their details using data in the database

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 using Vidly.Models;
 using Vidly.ViewModels;
 
@@ -10,38 +11,58 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        // GET: Movies
+        private ApplicationDbContext _context;
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();//GetMovies();
 
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek" },
-                new Movie { Id = 2, Name = "Wall-e" }
-            };
-        }
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                return HttpNotFound();
 
-        // GET: Movies/Random
-        public ActionResult Random()
-        {
-            var movie = new Movie() { Name = "Shrek!" };
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "John Smith" },
-                new Customer {Name = "Maria Sharapova" }
-            };
-
-            var viewModel = new RandomMovieViewModel
-            {
-                Movies = movie,
-                Customers = customers
-            };
-            return View(viewModel);
+            return View(movie);
         }
+        //private IEnumerable<Movie> GetMovies()
+        //{
+        //    return new List<Movie>
+        //    {
+        //        new Movie { Id = 1, Name = "Shrek" },
+        //        new Movie { Id = 2, Name = "Wall-e" }
+        //    };
+        //}
+
+        //// GET: Movies/Random
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie() { Name = "Shrek!" };
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer {Name = "John Smith" },
+        //        new Customer {Name = "Maria Sharapova" }
+        //    };
+
+        //    var viewModel = new RandomMovieViewModel
+        //    {
+        //        Movies = movie,
+        //        Customers = customers
+        //    };
+        //    return View(viewModel);
+        //}
     }
 }
